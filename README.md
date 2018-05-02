bfa12869155dd15d2569afd19825a48f6b67f45d
alexanderjaramillo4iep <alexanderjaramillo4iep@gmail.com>
2018-05-02 11:45:11 -0500

    Ejercicios clase 4 mayo

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # js-10-1
+
+## consola de comandos
+para crear una carpeta
+
+    mkdir nombre_de_la_carpeta
+para entrar en una carpeta
+
+    cd nombre_de_la_carpeta
+o si es una ruta más larga
+
+    cd /nombre/de/la/ruta/nombre_de_la_carpeta
+para ver el contenido de una carpeta
+
+    ls
+para ejecutar algun archivo js
+
+    node nombre_del_archivo
+
+## trabajado en clase
+    mkdir ejemplos
+    cd ejemplos
+    mkdir 1
+    cd 1
+luego de editar el archivo main.js, para probar utilizamos
+
+    node main.js
+
+## si se descarga este proyecto
+después de descomprimirlo abrir la carpeta con click derecho -> git bash
+
+entrar al archivo para probarlo
+
+    cd ejemplos/1/
+    node main.js
---
+++ ejercicios/1/main.js
@@ -0,0 +1,53 @@
+/**
+ * Función suma
+ * Dados dos números n1 y n2 retornar la suma
+ */
+function suma (n1, n2) {
+    return n1 + n2;
+}
+
+console.log(suma(4,9));
+;
+
+/**
+ * Función resta
+ * Dados dos números n1 y n2 retornar la resta
+ */
+function resta (n1, n2) {
+    //completar
+}
+console.log(resta(4,9));
+
+/**
+ * Función multiplicacion (sin tildes al programar)
+ * Dados dos números n1 y n2 retornar la multiplicación
+ */
+// Definir la función y hacer lo que dice la descripción
+// imprimir en consola la respuesta de la funcion multiplicar
+
+/**
+ * Función imprimirCarro
+ * Dado un objeto tipo carro imprimir (no retornar):
+ * nombre, modelo, peso, color
+ */
+function imprimirCarro(carro) {
+    console.log(carro.nombre, carro.modelo, carro.peso, carro.color);
+}
+const carro = {
+    nombre: "Fiat",
+    modelo: 2009,
+    peso: "900kg",
+    color: "azul",
+};
+imprimirCarro(carro);
+
+/**
+ * Función imprimirCarro2
+ * Dado un objeto tipo carro imprimir (no retornar):
+ * el vehículo carro.nombre de color carro.color modelo carro.modelo pesa carro.peso
+ */
+function imprimirCarro2(carro) {
+    //modificar la siguiente línea
+    console.log(carro.nombre, carro.modelo, carro.peso, carro.color);
+}
+// llamar la función imprimirCarro2
